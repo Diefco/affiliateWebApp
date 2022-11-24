4bba8d6eedb2ade22021256a9198f2cd5927a297
Diefco <diefco.web@gmail.com>
2022-11-23 23:11:35 -0500

    Nodemailer config

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-let dotenv = require('dotenv');
+const dotenv = require('dotenv');
 const cookieSession = require('cookie-session'); // invocamos morgan
 const morgan = require('morgan');
 
